711bc73d1951850d8dc3c86782fa363914ac5e43
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-09-07 11:41:20 +0300

    ss

--- days/dayTwo/indexTwoTests.js
+++ days/dayTwo/indexTwoTests.js
@@ -8,73 +8,73 @@ function timeline() {
             console.log(studySessionData);
             deleteFromSessionData();
             let updatedDates = updateDates();
-            // if ((updatedDates.fullDate.getDate() == updatedDates.yesterday.getDate()) && (studySessionData.subId != "64eda725d2c8fb647d0adc6e")) {
-            //     if (window.matchMedia("(orientation: landscape)").matches) {
-            //         document.getElementById("fiveAM").style.display = "inline";
-            //     } else {
-            //         document.getElementById("fiveAM_hor").style.display = "inline";
-            //     }
-            //     setTimeout(() => {
-            //         moveToDay();
-            //     }, timeToFive());
-            // }
-            // else if ((updatedDates.fullDate.getDate() == updatedDates.yesterdayPlusOne.getDate()) || (studySessionData.subId == "64eda725d2c8fb647d0adc6e")) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {)
-            //     if ((0 <= updatedDates.fullDate.getHours() & updatedDates.fullDate.getHours() < 5) && (studySessionData.subId != "64eda725d2c8fb647d0adc6e")) {
-            //         document.getElementById("fiveAM").style.display = "inline";
-            //         setTimeout(() => {
-            //             moveToDay();
-            //         }, timeToFiveSameDay());
-            //     } else {
-            let goTwo = async function () {
-                let my_awesome_script = document.createElement('script');
-                my_awesome_script.setAttribute('src', '../../functions/orientation.js');
-                // my_awesome_script.src = "../functions/orientation.js";
-                document.body.appendChild(my_awesome_script);
-                document.getElementById("startButton").style.display = "inline";
-                document.getElementById("redButton").style.display = "inline";
-                document.getElementById("blueButton").style.display = "inline";
-                document.getElementById("gameScreen").style.display = "inline";
-                document.getElementById("startButton").onclick = function () {
-                    document.getElementById("startButton").style.display = "none";
-                    studySessionData.doneDay2 = "stratDayTwo";
-                    platform.saveSession(studySessionData, true);
-                    getMillisec();
-                    let startTwoTests = async function () {
-                        let goTwoTests = async function () {
-                            // deleteFromSessionData();
-                            let doneTest1 = await start2tests(); // add promise and resolve
-                            if (doneTest1 == "done") {
-                                studySessionData.doneDay2 = 'doneDayTwo';
-                                studySessionData.expDaysDate = updatedDates.fullDate;
-                                platform.saveSession(studySessionData, true).then(() => {
-                                    document.getElementById("endDayMsg").style.display = "inline";
-                                    document.getElementById("endDayMsg").addEventListener("click", function () {
-                                        showWinnings()
-                                        setTimeout(() => {
-                                            platform.goToUrl("days/dayThree/dayThree.html");
-                                        }, timeToFive())
-                                        setTimeout(() => {
-                                            hideWinnings();
-                                            if (window.matchMedia("(orientation: landscape)").matches) {
-                                                document.getElementById("fiveAM").style.display = "inline";
-                                            } else {
-                                                document.getElementById("fiveAM_hor").style.display = "inline";
-                                            }
-                                        }, 10000);
-                                    })
-                                });
-                            }
-                        }
-                        goTwoTests()
-                    }
-                    startTwoTests()
+            if ((updatedDates.fullDate.getDate() == updatedDates.yesterday.getDate()) && (studySessionData.subId != "64eda725d2c8fb647d0adc6e")) {
+                if (window.matchMedia("(orientation: landscape)").matches) {
+                    document.getElementById("fiveAM").style.display = "inline";
+                } else {
+                    document.getElementById("fiveAM_hor").style.display = "inline";
                 }
+                setTimeout(() => {
+                    moveToDay();
+                }, timeToFive());
             }
-            goTwo();
-            // }
-            // } else {
-            //     document.getElementById("endOfGame").style.display = "inline";
-            // }
+            else if ((updatedDates.fullDate.getDate() == updatedDates.yesterdayPlusOne.getDate()) || (studySessionData.subId == "64eda725d2c8fb647d0adc6e")) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {)
+                if ((0 <= updatedDates.fullDate.getHours() & updatedDates.fullDate.getHours() < 5) && (studySessionData.subId != "64eda725d2c8fb647d0adc6e")) {
+                    document.getElementById("fiveAM").style.display = "inline";
+                    setTimeout(() => {
+                        moveToDay();
+                    }, timeToFiveSameDay());
+                } else {
+                    let goTwo = async function () {
+                        let my_awesome_script = document.createElement('script');
+                        my_awesome_script.setAttribute('src', '../../functions/orientation.js');
+                        // my_awesome_script.src = "../functions/orientation.js";
+                        document.body.appendChild(my_awesome_script);
+                        document.getElementById("startButton").style.display = "inline";
+                        document.getElementById("redButton").style.display = "inline";
+                        document.getElementById("blueButton").style.display = "inline";
+                        document.getElementById("gameScreen").style.display = "inline";
+                        document.getElementById("startButton").onclick = function () {
+                            document.getElementById("startButton").style.display = "none";
+                            studySessionData.doneDay2 = "stratDayTwo";
+                            platform.saveSession(studySessionData, true);
+                            getMillisec();
+                            let startTwoTests = async function () {
+                                let goTwoTests = async function () {
+                                    // deleteFromSessionData();
+                                    let doneTest1 = await start2tests(); // add promise and resolve
+                                    if (doneTest1 == "done") {
+                                        studySessionData.doneDay2 = 'doneDayTwo';
+                                        studySessionData.expDaysDate = updatedDates.fullDate;
+                                        platform.saveSession(studySessionData, true).then(() => {
+                                            document.getElementById("endDayMsg").style.display = "inline";
+                                            document.getElementById("endDayMsg").addEventListener("click", function () {
+                                                showWinnings()
+                                                setTimeout(() => {
+                                                    platform.goToUrl("days/dayThree/dayThree.html");
+                                                }, timeToFive())
+                                                setTimeout(() => {
+                                                    hideWinnings();
+                                                    if (window.matchMedia("(orientation: landscape)").matches) {
+                                                        document.getElementById("fiveAM").style.display = "inline";
+                                                    } else {
+                                                        document.getElementById("fiveAM_hor").style.display = "inline";
+                                                    }
+                                                }, 10000);
+                                            })
+                                        });
+                                    }
+                                }
+                                goTwoTests()
+                            }
+                            startTwoTests()
+                        }
+                    }
+                    goTwo();
+                }
+            } else {
+                document.getElementById("endOfGame").style.display = "inline";
+            }
         })
     })
 }
